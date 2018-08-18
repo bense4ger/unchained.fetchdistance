61f6c23740e7fee01a71f2414e4041c4d2482602
Ben Seager <github@benseager.co.uk>
2018-08-18 19:11:09 +0100

    handler implemented

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
 
+	"unchained.fetchdistance/api"
 	"unchained.fetchdistance/debug"
+	"unchained.fetchdistance/model"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -20,8 +25,67 @@ var (
 	dest   = flag.String("dest", "", "The journey's destination.  This should be an address")
 )
 
+func notOk(statusCode int, err error) (events.APIGatewayProxyResponse, error) {
+	h := make(map[string]string)
+	h["content-type"] = "application/json"
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       fmt.Sprintf("{\"message\":\"%s\"}", err.Error()),
+		Headers:    h,
+	}, err
+}
+
 func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{}, nil
+	c := api.Must(api.NewGMapsClient(apiKey))
+	if c == nil {
+		log.Warn("Failed to create API Client")
+		return notOk(http.StatusInternalServerError, fmt.Errorf("Failed to create API Client"))
+	}
+
+	if req.Body == "" {
+		log.Warn("Empty body passed")
+		return notOk(http.StatusBadRequest, fmt.Errorf("Empty body passed"))
+	}
+
+	bytes := []byte(req.Body)
+	ir := &model.IncomingRequest{}
+
+	err := json.Unmarshal(bytes, ir)
+	if err != nil {
+		log.Warnf("Error unmarshalling incoming request: %s", err.Error())
+		return notOk(http.StatusInternalServerError, fmt.Errorf("Unable to parse request body"))
+	}
+
+	o := &model.Location{
+		LatLong: ir.Origin,
+	}
+
+	d := &model.Location{
+		ID:      ir.ID,
+		LatLong: ir.Destination,
+	}
+
+	dist, err := c.GetDistance(context.Background(), o, d)
+	if err != nil {
+		log.Warnf("Error getting distance: %s", err.Error)
+		return notOk(http.StatusInternalServerError, fmt.Errorf("Unable to get distance"))
+	}
+
+	rb, err := json.Marshal(dist)
+	if err != nil {
+		log.Warnf("Error marshalling distance struct: %s", err.Error())
+		return notOk(http.StatusInternalServerError, fmt.Errorf("Unable to get distance"))
+	}
+
+	h := make(map[string]string)
+	h["content-type"] = "application/json"
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    h,
+		Body:       string(rb),
+	}, nil
 }
 
 func main() {
